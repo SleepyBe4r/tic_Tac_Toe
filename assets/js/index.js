03b734af02f4166a6c3302613b677f4b13e0cf32
SleepyBe4r <109447188+SleepyBe4r@users.noreply.github.com>
2023-04-26 17:05:58 -0300

    0.02

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,16 @@ var player_two_has_Symbol = false;
 var player_one_Symbol = 0;
 var player_two_Symbol = 0;
 
+var div_1a = "";
+var div_2a = "";
+var div_3a = "";
+var div_1b = "";
+var div_2b = "";
+var div_3b = "";
+var div_1c = "";
+var div_2c = "";
+var div_3c = "";
+
 function StartGame (btn_Selected,btn_Selected_Id){
     console.log(btn_Selected_Id);
     document.getElementById(btn_Selected_Id).style.display = "none";
@@ -27,23 +37,46 @@ function StartGame (btn_Selected,btn_Selected_Id){
 }
 
 function RoundStart(div_Selected_id) {
-    if (player_one_round == true) {
-        
 
+    var isEmpty = document.getElementById(div_Selected_id).innerHTML === "";
 
-        InsertSymbol(div_Selected_id, player_one_Symbol);
-
-        player_one_round = false;
-        player_two_round = true;
-    } else if (player_two_round == true){
-        
-        InsertSymbol(div_Selected_id, player_two_Symbol);
-
-        player_one_round = true;
-        player_two_round = false;
-    }
+    if (!isEmpty == "") {
+        if (player_one_round == true) {        
+    
+            CheckSymbol(div_Selected_id);
+            InsertSymbol(div_Selected_id, player_one_Symbol);
+    
+            player_one_round = false;
+            player_two_round = true;
+        } else if (player_two_round == true){
+            
+            CheckSymbol(div_Selected_id);
+            InsertSymbol(div_Selected_id, player_two_Symbol);
+    
+            player_one_round = true;
+            player_two_round = false;
+        }    
+    } 
+    
 
     EndRound();
+}
+
+function CheckSymbol(div_Selected_id) {
+    
+    switch (div_Selected_id) {
+        case "box_C_1a":
+            
+            break;
+        case "box_C_1b":
+            
+            break;
+        case "box_C_1c":
+            
+            break;
+        default:
+            break;
+    }
 }
 
 function InsertSymbol(div_Selected_id, player_symbol) {
